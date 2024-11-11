46c7f53da679a155d2a368bb29b47774929cca85
gavinarori <arorigavin@gmail.com>
2024-11-11 13:29:34 +0300

    sign in and up uis

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -4,9 +4,6 @@ import React from 'react'
 const SignInPage = () => {
   return (
     <div className='flex flex-col gap-4'>
-        <h1 className='text-3xl'>
-            Sign In
-        </h1>
         <SignInForm />
     </div>
   )
--- src/app/components/SignInForm.tsx
+++ src/app/components/SignInForm.tsx
@@ -46,14 +46,57 @@ const SignInForm = () => {
     }, [status]);
 
     return (
-        <div className='flex flex-col gap-4 bg-gray-400 p-4'>
-            <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center w-full">
+        <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+            <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+                <div className="mt-12 flex flex-col items-center">
+                    <h1 className="text-2xl xl:text-3xl font-extrabold">
+                        Sign in
+                    </h1>
+                    <div className="w-full flex-1 mt-8">
+                        <div className="mx-auto max-w-xs">
+                            <input
+                                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-200 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                type="email" placeholder="Email" />
+                            <input
+                                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-200 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                type="password" placeholder="Password" />
 
-            <button onClick={handleSubmit}>Sign in</button>
-
-            <p>{message}</p>
+                            <a href="#" className="border-b mt-2 border-gray-500 text-blue-600 border-dotted">
+                              forgot your password?
+                          </a>
+                            <button
+                                className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth="2"
+                                    strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                    <circle cx="8.5" cy="7" r="4" />
+                                    <path d="M20 8v6M23 11h-6" />
+                                </svg>
+                                <span className="ml-3">
+                                    Sign in
+                                </span>
+                            </button>
+                            <p className="mt-6 text-xs text-gray-600 text-center">
+                                Don't have an account ?
+                                <a href="/auth/signup" className="border-b border-gray-500 text-blue-500 border-dotted">
+                                    sign up
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1 bg-gray-200 text-center hidden lg:flex">
+        <div 
+          className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('https://img.freepik.com/free-vector/telecommuting-concept-smiley-man_23-2148497411.jpg?t=st=1728301153~exp=1728304753~hmac=6c72b780cb73fdf86c7b7901a105910724d930fe2ba5b12295d43471b4ed3c7f&w=740')" }}
+        >
         </div>
+      </div>
+      
+        </div>
+      </div>
     );
 };
 
